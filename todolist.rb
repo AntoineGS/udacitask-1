a463aac558522539a92baf51945e3aff763fe584
AntoineGS <antoineg.simard@gmail.com>
2016-01-30 20:03:10 -0500

    Added add_item method and Item class initialize method

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,23 @@
 class TodoList
-    # methods and stuff go here
+  # methods and stuff go here
+  attr_accessor :title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new # Starts empty! No Items yet!
+  end
+
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
 end
 
 class Item
-    # methods and stuff go here
+  # methods and stuff go here
+  attr_accessor :description, :completion
+
+  def initialize(item_description)
+    @description = item_description
+    @completion = false
+  end
 end
