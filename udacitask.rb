3da0f07934a77e4184793ad87dc5089485a759da
AntoineGS <antoineg.simard@gmail.com>
2016-03-15 20:17:40 -0400

    Added creation of list and items to todolist.rb

--- udacitask.rb
+++ udacitask.rb
@@ -1,9 +1,12 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-
+job = TodoList.new("job")
 # Add four new items
-
+job.add_item("10AM meeting with Bob")
+job.add_item("11AM call with Jhonny")
+job.add_item("Finish documentation")
+job.add_item("4PM leave the office silently")
 # Print the list
 
 # Delete the first item
